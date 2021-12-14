a89a64e30c4031dfd7a48979e0e187b41ccb7ece
Mauro Quinteros <quinterosmauro0599@gmail.com>
2021-12-14 16:56:58 -0500

    [ADD] :sparkles: Primer functional component && Agregar estilos

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-const App = <h1>Hello World!</h1>;
-console.log(App);
+import "./index.css";
 
-ReactDOM.render(App, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
