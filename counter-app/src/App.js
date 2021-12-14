5813ebaf5adf2721020776d7e5bd0a66bb4557a9
Mauro Quinteros <quinterosmauro0599@gmail.com>
2021-12-14 17:15:00 -0500

    [ADD] :sparkles: Imprimir variables en el HTML

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  const greeting = "Hello World!";
+  const isFalse = true; // No lo imprime
+  const numberList = [1, 2, 3, 4]; // Lo imprime como string
+  const person = {
+    // Tengo que convertirlo a string para imprimirlo y usar la etiqueta pre
+    name: "Mauro",
+    age: 22,
+  };
+
+  return (
+    <>
+      <h1>{greeting}</h1>
+      <p>{isFalse}</p>
+      <p>{numberList}</p>
+      <pre>{JSON.stringify(person, null, 3)}</pre>
+      <p>My First Component</p>
+    </>
+  );
 };
 
 export default App;
