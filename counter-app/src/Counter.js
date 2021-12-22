f5257373368be4fbc40514238e770d26a89a81e4
Mauro Quinteros <quinterosmauro0599@gmail.com>
2021-12-22 15:58:25 -0500

    [ADD] :sparkles: Agregar el event click

--- counter-app/src/Counter.js
+++ counter-app/src/Counter.js
@@ -2,10 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Counter = ({ value }) => {
+  const handleAdd = (ev) => {
+    console.log(ev);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+      <button onClick={handleAdd}>Add</button>
     </>
   );
 };
