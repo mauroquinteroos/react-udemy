7caa5b855f3fcb295ce4fe07fa8796842339d5a1
Mauro Quinteros <quinterosmauro0599@gmail.com>
2022-02-18 15:00:32 -0500

    [ADD] :sparkles: Agregar build de GifExpertApp

---
+++ docs/precache-manifest.6938bfab68b7da36b3dec7231ba5b5bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd7d02d1d8be6ed09de88c01f7e930b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "146b127c6fb7182a4cf7",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "e004ab1f28cafd6208cb",
+    "url": "/static/css/main.f266119e.chunk.css"
+  },
+  {
+    "revision": "146b127c6fb7182a4cf7",
+    "url": "/static/js/2.7edd6806.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7edd6806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e004ab1f28cafd6208cb",
+    "url": "/static/js/main.03b7a664.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
